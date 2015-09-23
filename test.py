e841e5f435ecdc098d96b35cf0431c822ef57648
Meng Zhuo <mengzhuo1203@gmail.com>
2015-09-23 22:28:33 +0800

    add docs

---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import unittest
+
+class TestJustRPCServer(unittest.TestCase):
+
+    """Test case docstring."""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_name(self):
+        pass
